364b4c2ce4af3a9a47989d88ebd6b8a843abd1ee
Dou <47zrjrv4tv@privaterelay.appleid.com>
2026-09-10 23:38:40 +0800

    1.0.4: fix Custom icons input box (type text -> input)

--- extension.js
+++ extension.js
@@ -177,8 +177,8 @@ function onload(input) {
         {
           id: 'customIcons',
           name: 'Custom icons (one per line)',
-          description: 'Format: domain=image-url  e.g. github.com=https://github.com/favicon.ico',
-          action: { type: 'text', placeholder: 'github.com=https://github.com/favicon.ico', onChange: reapplyAll },
+          description: 'One per line. Format: domain=image-url  e.g. github.com=https://github.com/favicon.ico',
+          action: { type: 'input', placeholder: 'github.com=https://github.com/favicon.ico', onChange: reapplyAll },
         },
         {
           id: 'fallback',
